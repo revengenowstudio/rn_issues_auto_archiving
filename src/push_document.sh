89505576c6980cb8f69ac9f2658a3ea0f58e7ebb
shuiping233 <1944680304@qq.com>
2024-11-18 21:20:36 +0800

    试图解决归档commit不能读取issue单号的bug

--- src/push_document.sh
+++ src/push_document.sh
@@ -6,6 +6,6 @@ if [[ -z $diff ]]; then
     echo "归档文档没有发生变化，跳过提交流程"
 else
     echo "归档文档发生了变化，执行提交流程"
-    git commit -m "$COMMIT_TITLE$ISSUE_NUMBER"
+    git commit -m "$COMMIT_TITLE$ISSUE_NUMBER$MANUAL_ISSUE_NUMBER"
     git push origin main
 fi
