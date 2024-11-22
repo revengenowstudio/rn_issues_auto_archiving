4e995919424318cc9bb2c5dbfa5ff6cfed1a50cf
shuiping233 <1944680304@qq.com>
2024-11-22 09:41:16 +0800

    修复了流水线启动失败的bug

--- src/shared/get_args.py
+++ src/shared/get_args.py
@@ -1,9 +1,9 @@
 import sys
 
 def get_value_from_args(short_arg: str,
-                        long_arg: str) -> str:
+                        long_arg: str) -> str|None:
     argv = sys.argv
-    result = ""
+    result = None
     if (long_arg in argv):
         result = argv[argv.index(long_arg) + 1]
     if (short_arg in argv != -1):
