9f32334ba9e518389c956ca1378cea591622838f
shuiping233 <1944680304@qq.com>
2024-11-23 20:18:08 +0800

    更新output脚本

--- output.ps1
+++ output.ps1
@@ -1,4 +1,5 @@
 # 创建输出目录，如果已经存在则不报错
+Remove-Item -Recurse -Force -Path "./output/*"
 New-Item -ItemType Directory -Force -Path "./output"
 
 # 复制文件和目录
@@ -7,6 +8,12 @@ Copy-Item -Recurse -Force -Verbose "./.github" -Destination "./output/.github"
 Copy-Item -Recurse -Force -Verbose "./.gitlab" -Destination "./output/.gitlab"
 Copy-Item -Force -Verbose "./.gitlab-ci.yml" -Destination "./output/.gitlab-ci.yml"
 Copy-Item -Recurse -Force -Verbose "./rn_issue_auto_archiving" -Destination "./output/rn_issue_auto_archiving"
+
+Remove-Item -Recurse -Force -Path "./output/rn_issue_auto_archiving/__pycache__"
+Remove-Item -Recurse -Force -Path "./output/rn_issue_auto_archiving/auto_archiving/__pycache__"
+Remove-Item -Recurse -Force -Path "./output/rn_issue_auto_archiving/issue_processor/__pycache__"
+Remove-Item -Recurse -Force -Path "./output/rn_issue_auto_archiving/shared/__pycache__"
+Remove-Item -Recurse -Force -Path "./output/rn_issue_auto_archiving/utils/__pycache__"
 
 # 输出完成信息
 Write-Output "output done"
--- output.sh
+++ output.sh
@@ -1,3 +1,4 @@
+rm -rf ./output
 mkdir -p ./output
 
 cp -frv ./config ./output/config
@@ -5,5 +6,11 @@ cp -frv ./.github ./output/.github
 cp -frv ./.gitlab ./output/.gitlab
 cp -frv ./.gitlab-ci.yml ./output/.gitlab-ci.yml
 cp -frv ./rn_issue_auto_archiving ./output/rn_issue_auto_archiving
+
+rm -rf ./output/rn_issue_auto_archiving/__pycache__
+rm -rf ./output/rn_issue_auto_archiving/auto_archiving/__pycache__
+rm -rf ./output/rn_issue_auto_archiving/issue_processor/__pycache__
+rm -rf ./output/rn_issue_auto_archiving/shared/__pycache__
+rm -rf ./output/rn_issue_auto_archiving/utils/__pycache__
 
 echo "output done"
