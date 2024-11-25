195834076a408de04687c02148b57aea2d31fca6
shuiping233 <1944680304@qq.com>
2024-11-25 14:05:44 +0800

    搓好了3个方便部署流水线和流水线所需脚本的部署脚本

--- output.sh
+++ output.sh
@@ -1,81 +1,107 @@
 #!/bin/bash
 
-# 创建输出目录，如果已经存在则不报错
-rm -rf ./output/*
-mkdir -p ./output
+set -e
+function msg { out "$*" >&2; }
+function err {
+    local x=$?
+    msg "$*"
+    return $(($x == 0 ? 1 : $x))
+}
+function out { printf '%s\n' "$*"; }
 
-# 复制文件和目录
-cp -r ./config ./output/
-cp -r ./.github ./output/
-cp -r ./.gitlab ./output/
-cp ./.gitlab-ci.yml ./output/
-cp -r ./rn_issue_auto_archiving ./output/
+function globals {
+    this="./output"
+    RN_ALL_ISSUES_REPO_PATH=""
+    RN_INTERNAL_ISSUES_REPO_PATH=""
+    outputPath=""
+}
+globals
 
-# 删除特定目录
-rm -rf ./output/rn_issue_auto_archiving/__pycache__
-rm -rf ./output/rn_issue_auto_archiving/auto_archiving/__pycache__
-rm -rf ./output/rn_issue_auto_archiving/issue_processor/__pycache__
-rm -rf ./output/rn_issue_auto_archiving/shared/__pycache__
-rm -rf ./output/rn_issue_auto_archiving/utils/__pycache__
-rm -rf ./output/.gitlab/workflows/DeployAutoArchiving.yml
-rm -rf ./output/.github/workflows/DeployAutoArchiving.yml
+function parseCmds {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in # Munging globals, beware
+        --RN_ALL_ISSUES_REPO_PATH)
+            RN_ALL_ISSUES_REPO_PATH="$2"
+            shift 2
+            ;;
+        --RN_INTERNAL_ISSUES_REPO_PATH)
+            RN_INTERNAL_ISSUES_REPO_PATH="$2"
+            shift 2
+            ;;
+        -o)
+            outputPath="$2"
+            shift 2
+            ;;
+        *) err 'Argument error. Please see help.' ;;
+        esac
+    done
+    : "${outputPath:=$this}"
+}
+
+function pickFiles {
+    # 创建输出目录，如果已经存在则不报错
+    echo "pick files from repository to '$outputPath'"
+    rm -rf "$outputPath/*"
+    mkdir -p "$outputPath"
+
+    # 复制文件和目录
+    cp -r "./config" "$outputPath/"
+    cp -r "./.github" "$outputPath/"
+    cp -r "./.gitlab" "$outputPath/"
+    cp "./.gitlab-ci.yml" "$outputPath/"
+    cp -r "./rn_issue_auto_archiving" "$outputPath/"
+
+    # 删除特定目录
+    rm -rf "$outputPath/rn_issue_auto_archiving/__pycache__"
+    rm -rf "$outputPath/rn_issue_auto_archiving/auto_archiving/__pycache__"
+    rm -rf "$outputPath/rn_issue_auto_archiving/issue_processor/__pycache__"
+    rm -rf "$outputPath/rn_issue_auto_archiving/shared/__pycache__"
+    rm -rf "$outputPath/rn_issue_auto_archiving/utils/__pycache__"
+    rm -rf "$outputPath/.gitlab/workflows/DeployAutoArchiving.yml"
+    rm -rf "$outputPath/.github/workflows/DeployAutoArchiving.yml"
+}
 
 # 定义一个函数来处理文件
-update_branch_in_file() {
+function updateBranchInFile() {
     local file_name="$1"
     local content=$(<"$file_name")
     local updated_content=$(echo "$content" | sed 's/TARGET_BRANCH:[[:space:]]*main/TARGET_BRANCH: master/g')
-    echo "$updated_content" > "$file_name"
-    echo "File updated: $file_name"
+    echo "$updated_content" >"$file_name"
+    echo "File updated : '$file_name' , TARGET_BRANCH value update to 'master'"
 }
 
-# 定义一个函数来检查并返回指定参数的值
-get_command_line_parameter() {
-    local args=("$@")
-    local parameter_name="$2"
-    local parameter_value=""
-
-    for (( i=0; i<${#args[@]}; i++ )); do
-        if [[ "${args[i]}" == "$parameter_name" ]]; then
-            if (( i+1 < ${#args[@]} )); then
-                parameter_value="${args[i+1]}"
-                break
-            else
-                echo "Error: Missing value for $parameter_name"
-                exit 1
-            fi
-        fi
-    done
-
-    if [[ -z "$parameter_value" ]]; then
-        echo "Error: Parameter $parameter_name not found or invalid."
-        exit 1
-    fi
-
-    echo $parameter_value
+function copyOutputFilesToAllIssueRepo {
+    # 复制文件和目录到目标路径
+    cp -r "$outputPath/config" "$RN_ALL_ISSUES_REPO_PATH"
+    cp -r "$outputPath/.github" "$RN_ALL_ISSUES_REPO_PATH/"
+    cp -r "$outputPath/rn_issue_auto_archiving" "$RN_ALL_ISSUES_REPO_PATH/"
 }
 
-# 调用函数处理第一个文件
-update_branch_in_file "./output/.github/workflows/AutoArchiving.yml"
+function copyOutputFilesToInternalIssueRepo {
+    cp -r "$outputPath/config" "$RN_INTERNAL_ISSUES_REPO_PATH"
+    cp -r "$outputPath/.gitlab" "$RN_INTERNAL_ISSUES_REPO_PATH/"
+    cp -r "$outputPath/.gitlab-ci.yml" "$RN_INTERNAL_ISSUES_REPO_PATH/.gitlab-ci.yml"
+    cp -r "$outputPath/rn_issue_auto_archiving" "$RN_INTERNAL_ISSUES_REPO_PATH/"
+}
 
-# 调用函数处理第二个文件
-update_branch_in_file "./output/.gitlab/workflows/AutoArchiving.yml"
+parseCmds "$@"
+pickFiles
+updateBranchInFile "$outputPath/.github/workflows/AutoArchiving.yml"
+updateBranchInFile "$outputPath/.gitlab/workflows/AutoArchiving.yml"
 
-RN_ALL_ISSUES_REPO_PATH=$(get_command_line_parameter "$@" "--RN_ALL_ISSUES_REPO_PATH")
-echo "$RN_ALL_ISSUES_REPO_PATH"
+if [[ -n "$RN_ALL_ISSUES_REPO_PATH" ]]; then
+    echo "get RN_ALL_ISSUES_REPO_PATH '$RN_ALL_ISSUES_REPO_PATH' , copy output files to '$RN_ALL_ISSUES_REPO_PATH'"
+    copyOutputFilesToAllIssueRepo
+else
+    echo "--RN_ALL_ISSUES_REPO_PATH is empty"
+fi
 
-RN_INTERNAL_ISSUES_REPO_PATH=$(get_command_line_parameter "$@" "--RN_INTERNAL_ISSUES_REPO_PATH")
-echo "$RN_INTERNAL_ISSUES_REPO_PATH"
-
-# 复制文件和目录到目标路径
-cp -r ./output/config "$RN_ALL_ISSUES_REPO_PATH"
-cp -r ./output/.github "$RN_ALL_ISSUES_REPO_PATH/"
-cp -r ./output/rn_issue_auto_archiving "$RN_ALL_ISSUES_REPO_PATH/"
-
-cp -r ./output/config "$RN_INTERNAL_ISSUES_REPO_PATH"
-cp -r ./output/.gitlab "$RN_INTERNAL_ISSUES_REPO_PATH/"
-cp ./output/.gitlab-ci.yml "$RN_INTERNAL_ISSUES_REPO_PATH/.gitlab-ci.yml"
-cp -r ./output/rn_issue_auto_archiving "$RN_INTERNAL_ISSUES_REPO_PATH/"
+if [[ -n "$RN_INTERNAL_ISSUES_REPO_PATH" ]]; then
+echo "get RN_INTERNAL_ISSUES_REPO_PATH '$RN_INTERNAL_ISSUES_REPO_PATH' , copy output files to '$RN_INTERNAL_ISSUES_REPO_PATH'"
+    copyOutputFilesToInternalIssueRepo
+else
+    echo "--RN_INTERNAL_ISSUES_REPO_PATH is empty"
+fi
 
 # 输出完成信息
 echo "output done"
