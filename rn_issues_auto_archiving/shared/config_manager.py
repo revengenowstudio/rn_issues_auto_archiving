f32439d37a62c357d94f3f770d5b4612c37f5bb5
shuiping233 <1944680304@qq.com>
2024-12-12 20:02:21 +0800

    完成了 config_manager 的单元测试

--- rn_issues_auto_archiving/shared/config_manager.py
+++ rn_issues_auto_archiving/shared/config_manager.py
@@ -1,5 +1,3 @@
-from dataclasses import replace
-
 from shared.data_source import DataSource
 
 
@@ -10,11 +8,11 @@ class ConfigManager():
     ):
         if data_sources is None:
             data_sources = []
-        self.data_sources = data_sources
+        self.__data_sources = data_sources
 
     def register_data_source(self, data_source: DataSource):
-        self.data_sources.append(data_source)
+        self.__data_sources.append(data_source)
 
     def load_all(self, config: object):
-        for data_source in self.data_sources:
+        for data_source in self.__data_sources:
             data_source.load(config)
