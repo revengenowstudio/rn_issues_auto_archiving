ddf6bac4c2b55cd5d41e00dada2f506b165361af
shuiping233 <1944680304@qq.com>
2024-12-15 21:34:16 +0800

    更新powershell输出脚本；暂时放弃nu和sh的输出脚本的开发和使用

--- script/output.ps1
+++ script/output.ps1
@@ -17,6 +17,25 @@ function Update-BranchInFile {
     # 输出完成信息
     Write-Host "File updated: $fileName"
 }
+function Remove-UnittestInFile {
+    param (
+        [string]$fileName
+    )
+
+    # 使用 UTF-8 编码读取输入文件内容
+    $content = [System.IO.File]::ReadAllText($fileName, [System.Text.Encoding]::UTF8)
+    # 删除 "unittest" 阶段
+    $fileContent = $content -replace "(?m)^\s*- unittest\s*$", ""
+
+    # 删除 "- local: \"/.gitlab/workflows/Unittest.yml\"" 行
+    $fileContent = $fileContent -replace "(?m)^\s*- local: `"/.gitlab/workflows/Unittest.yml`"\s*$", ""
+
+    # 将更新后的内容写回原文件，使用 UTF-8 编码
+    [System.IO.File]::WriteAllText($fileName, $fileContent, [System.Text.Encoding]::UTF8)
+
+    # 输出完成信息
+    Write-Host "File updated: $fileName"
+}
 
 function Add-IncludeInGitlabCi {
     param (
@@ -52,7 +71,8 @@ function Get-CommandLineParameter {
             if ($i + 1 -lt $cmdArgs.Length) {
                 $parameterValue = $cmdArgs[$i + 1]
                 break
-            } else {
+            }
+            else {
                 Write-Host "Error: Missing value for $parameterName"
                 exit 1
             }
@@ -63,7 +83,8 @@ function Get-CommandLineParameter {
     if (-not $parameterValue) {
         Write-Host "Error: Parameter $parameterName not found or invalid."
         exit 1
-    }else {
+    }
+    else {
         Write-Host "Get Parameter '$parameterName' , value : $parameterValue"
     }    
     # 返回参数值
@@ -71,16 +92,19 @@ function Get-CommandLineParameter {
 }
 
 # 创建输出目录，如果已经存在则不报错
+Write-Host "clear output dir"
 Remove-Item -Recurse -Force -Path "./output/*"
 New-Item -ItemType Directory -Force -Path "./output"
 
 # 复制文件和目录
-Copy-Item -Recurse -Force -Verbose "./config" -Destination "./output/config"
-Copy-Item -Recurse -Force -Verbose "./.github" -Destination "./output/.github"
-Copy-Item -Recurse -Force -Verbose "./.gitlab" -Destination "./output/.gitlab"
-Copy-Item -Force -Verbose "./.gitlab-ci.yml" -Destination "./output/.gitlab-ci.yml"
-Copy-Item -Recurse -Force -Verbose "./rn_issues_auto_archiving" -Destination "./output/rn_issues_auto_archiving"
+Write-Host "copy file to output dir"
+Copy-Item -Recurse -Force "./config" -Destination "./output/config"
+Copy-Item -Recurse -Force "./.github" -Destination "./output/.github"
+Copy-Item -Recurse -Force "./.gitlab" -Destination "./output/.gitlab"
+Copy-Item -Force "./.gitlab-ci.yml" -Destination "./output/.gitlab-ci.yml"
+Copy-Item -Recurse -Force "./rn_issues_auto_archiving" -Destination "./output/rn_issues_auto_archiving"
 
+Write-Host "remove unnecessary files"
 Remove-Item -Recurse -Force -Path "./output/rn_issues_auto_archiving/__pycache__"
 Remove-Item -Recurse -Force -Path "./output/rn_issues_auto_archiving/auto_archiving/__pycache__"
 Remove-Item -Recurse -Force -Path "./output/rn_issues_auto_archiving/issue_processor/__pycache__"
@@ -88,6 +112,10 @@ Remove-Item -Recurse -Force -Path "./output/rn_issues_auto_archiving/shared/__py
 Remove-Item -Recurse -Force -Path "./output/rn_issues_auto_archiving/utils/__pycache__"
 Remove-Item -Recurse -Force -Path "./output/.gitlab/workflows/DeployAutoArchiving.yml"
 Remove-Item -Recurse -Force -Path "./output/.github/workflows/DeployAutoArchiving.yml"
+Remove-Item -Recurse -Force -Path "./output/.github/workflows/Unittest.yml"
+Remove-Item -Recurse -Force -Path "./output/.gitlab/workflows/Unittest.yml"
+Remove-Item -Recurse -Force -Path "./output/rn_issues_auto_archiving/tests"
+Remove-Item -Recurse -Force -Path "./output/rn_issues_auto_archiving/test.py"
 
 # 调用函数处理第一个文件
 Update-BranchInFile -fileName "./output/.github/workflows/AutoArchiving.yml"
@@ -95,23 +123,32 @@ Update-BranchInFile -fileName "./output/.github/workflows/AutoArchiving.yml"
 # 调用函数处理第二个文件
 Update-BranchInFile -fileName "./output/.gitlab/workflows/AutoArchiving.yml"
 
+Remove-UnittestInFile -fileName "./output/.gitlab-ci.yml"
+
 Add-IncludeInGitlabCi -fileName "./output/.gitlab-ci.yml"
 
 # 主程序入口
 $RN_ALL_ISSUES_REPO_PATH = Get-CommandLineParameter -parameterName "--RN_ALL_ISSUES_REPO_PATH" -cmdArgs $args
-Write-Host "$RN_ALL_ISSUES_REPO_PATH"
 
 $RN_INTERNAL_ISSUES_REPO_PATH = Get-CommandLineParameter -parameterName "--RN_INTERNAL_ISSUES_REPO_PATH" -cmdArgs $args
-Write-Host "$RN_INTERNAL_ISSUES_REPO_PATH"
 
-Copy-Item -Recurse -Force -Verbose "./output/config" -Destination "$RN_ALL_ISSUES_REPO_PATH"
-Copy-Item -Recurse -Force -Verbose "./output/.github" -Destination "$RN_ALL_ISSUES_REPO_PATH/"
-Copy-Item -Recurse -Force -Verbose "./output/rn_issues_auto_archiving" -Destination "$RN_ALL_ISSUES_REPO_PATH/"
+Write-Host "copy output file to `"$RN_ALL_ISSUES_REPO_PATH`""
+Remove-Item -Recurse -Force "$RN_ALL_ISSUES_REPO_PATH/config"
+Remove-Item -Recurse -Force "$RN_ALL_ISSUES_REPO_PATH/rn_issues_auto_archiving"
 
-Copy-Item -Recurse -Force -Verbose "./output/config" -Destination "$RN_INTERNAL_ISSUES_REPO_PATH"
-Copy-Item -Recurse -Force -Verbose "./output/.gitlab" -Destination "$RN_INTERNAL_ISSUES_REPO_PATH/"
-Copy-Item -Force -Verbose "./output/.gitlab-ci.yml" -Destination "$RN_INTERNAL_ISSUES_REPO_PATH/.gitlab-ci.yml"
-Copy-Item -Recurse -Force -Verbose "./output/rn_issues_auto_archiving" -Destination "$RN_INTERNAL_ISSUES_REPO_PATH/"
+Copy-Item -Recurse -Force "./output/config" -Destination "$RN_ALL_ISSUES_REPO_PATH"
+Copy-Item -Recurse -Force "./output/.github" -Destination "$RN_ALL_ISSUES_REPO_PATH/"
+Copy-Item -Recurse -Force "./output/rn_issues_auto_archiving" -Destination "$RN_ALL_ISSUES_REPO_PATH/"
+
+Write-Host "copy output file to `"$RN_INTERNAL_ISSUES_REPO_PATH`""
+Remove-Item -Recurse -Force "$RN_INTERNAL_ISSUES_REPO_PATH/config"
+Remove-Item -Force "$RN_INTERNAL_ISSUES_REPO_PATH/.gitlab-ci.yml"
+Remove-Item -Recurse -Force "$RN_INTERNAL_ISSUES_REPO_PATH/rn_issues_auto_archiving"
+
+Copy-Item -Recurse -Force "./output/config" -Destination "$RN_INTERNAL_ISSUES_REPO_PATH"
+Copy-Item -Recurse -Force "./output/.gitlab" -Destination "$RN_INTERNAL_ISSUES_REPO_PATH/"
+Copy-Item -Force "./output/.gitlab-ci.yml" -Destination "$RN_INTERNAL_ISSUES_REPO_PATH/.gitlab-ci.yml"
+Copy-Item -Recurse -Force "./output/rn_issues_auto_archiving" -Destination "$RN_INTERNAL_ISSUES_REPO_PATH/"
 
 # 输出完成信息
 Write-Host "output done"
