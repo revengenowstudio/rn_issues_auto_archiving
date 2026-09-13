9b3f5866b28aa58f384a126d8c65e3452c96c6f3
shuiping233 <1944680304@qq.com>
2026-09-13 23:19:23 +0800

    chore: missing_archive_version 加换行

--- rn_issues_auto_archiving/shared/exception.py
+++ rn_issues_auto_archiving/shared/exception.py
@@ -8,7 +8,7 @@ class ErrorMessage:
     匹配到的版本号有：{versions}
     """
 
-    missing_archive_version = """Issue评论中找不到归档版本号关键字，请确保Issue评论中归档关键字格式正确且包含归档版本号。补全必要信息后请再次关闭Issue重新触发归档流程。
+    missing_archive_version = """Issue评论中找不到归档版本号关键字，请确保Issue评论中归档关键字格式正确且包含归档版本号。补全必要信息后请再次关闭Issue重新触发归档流程。  
     归档关键字格式有：
 {hints}"""
 
